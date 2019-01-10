ea086ab225bfa0961f2096e4245afe2a6779fd2d
QMENG <damon.q.meng@gmail.com>
2019-01-10 15:23:39 -0500

    Initial Commit

---
+++ Instagramimic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagramimic
+//
+//  Created by Damon on 2019-01-10.
+//  Copyright © 2019 damonmeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
