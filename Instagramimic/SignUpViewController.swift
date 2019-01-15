5f58a612de4658d595cc678d97b258354b899304
QMENG <damon.q.meng@gmail.com>
2019-01-14 21:15:38 -0500

    added code for camera usage

---
+++ Instagramimic/SignUpViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SignUpViewController.swift
+//  Instagramimic
+//
+//  Created by Damon on 2019-01-14.
+//  Copyright © 2019 damonmeng. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var shortBioTextField: UITextField!
+    
+    var imagePickerController : UIImagePickerController!
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBAction func takePhoto(_ sender: Any) {
+        imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .camera
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imagePickerController.dismiss(animated: true, completion: nil)
+        profileImage.image = info[.originalImage] as? UIImage
+    }
+
+}
